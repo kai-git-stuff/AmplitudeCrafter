785db84e08a6f4084cc6d4ab94a44d21d3618004
Kai Habermann <kai.habermann@gmx.net>
2022-06-07 09:34:37 +0200

    Added decay example to known paths

--- AmplitudeCrafter/locals.py
+++ AmplitudeCrafter/locals.py
@@ -3,4 +3,5 @@ import sys
 main_dir = dirname(dirname(abspath(__file__)))
 src_dir = dirname(abspath(__file__))
 config_dir = join(src_dir,"config/")
+decay_example = join(config_dir,"decay_example.yml")
 particle_config = join(config_dir,"particles.yml")
