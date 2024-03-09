61a554bcf03474b566f507c1454d20c7f9e8ba86
Kai Habermann <kai.habermann@gmx.net>
2024-03-09 21:15:11 +0100

    ...

--- AmplitudeCrafter/helpers.py
+++ AmplitudeCrafter/helpers.py
@@ -4,6 +4,8 @@ from jitter.constants import spin as sp
 from jitter.kinematics import clebsch
 from AmplitudeCrafter.parameters import parameter, lambdaParameter
 import warnings
+from jitter.kinematics import helicity_couplings_from_ls
+import numpy as np
 
 def is_free(p):
     return not p.const
@@ -20,30 +22,41 @@ def ensure_numeric(p):
         p = p()
     return float(p)
 
-def get_maxLS_prefactors(mother: particle, child: particle, massless_child: particle, ls: set, lmax: int, smax: int) -> dict:
+def coeffLS(L, S, mother: particle, resonance: particle, gamma: particle): # ANJA Jan 24
+    # Clebsch for lambda_Lambda=+-1/2, lambda_gamma=0 of res
+    cP = clebsch(L,0,S, 1,mother.spin, 1)*clebsch(resonance.spin, 1,gamma.spin,0,S, 1)
+    cM = clebsch(L,0,S,-1,mother.spin,-1)*clebsch(resonance.spin,-1,gamma.spin,0,S,-1)
+    return np.sqrt((L+1)/(mother.spin+1)) * cP, np.sqrt((L+1)/(mother.spin+1)) * cM
+
+def get_maxLS_prefactors(mother: particle, resonance: particle, gamma: particle, ls: set, lmax: int, smax: int) -> dict:
     # other daughter is assumed to be photon
     def cs(L, S, m_lambda):
-        return clebsch(2, 0, child.spin, m_lambda, S, 0 + m_lambda) * clebsch(L, 0, S, 1, mother.spin, 0 + m_lambda)
-    prefactors_ls_max = {}
-    for (L, S) in ls:
-        if L == lmax:
-            prefactors_ls_max[(L, S)] = 0
-            continue
-        prefactors_ls_max[(L, S)] = cs(L, S, 1) - cs(L, S, -1) / (cs(lmax, smax, 1) - cs(lmax, smax, -1))
+        return clebsch(2, 0, resonance.spin, m_lambda, S, 0 + m_lambda) * clebsch(L, 0, S, 1, mother.spin, 0 + 1)
 
+    # highest L and S replacement
+    Cp_sl, Cm_sl = coeffLS(lmax-2,smax, mother, resonance, gamma)
+    Cp_l,  Cm_l  = coeffLS(lmax,smax, mother, resonance, gamma)
+
+    factor_second_highest = -1.0 / (Cp_sl-(Cp_l/Cm_l) * Cm_sl)
     prefactors_ls_max_minus = {}
     for (L, S) in ls:
         if S == smax:
             prefactors_ls_max_minus[(L, S)] = 0
             continue
-        factor = 1 / (
-            (cs(lmax - 2, smax, 1) - cs(lmax - 2, smax, -1)) / (cs(lmax, smax, 1) - cs(lmax, smax, -1)) +
-            (cs(lmax - 2, smax, 1) + cs(lmax - 2, smax, -1)) / (cs(lmax, smax, 1) + cs(lmax, smax, -1))
-        )
-        prefactors_ls_max_minus[(L, S)] = factor * (
-            (cs(L, S, 1) - cs(L, S, -1)) / (cs(lmax, smax, 1) - cs(lmax, smax, -1)) +
-            (cs(L, S, 1) + cs(L, S, -1)) / (cs(lmax, smax, 1) + cs(lmax, smax, -1))
-        )
+        Cp_LS, Cm_LS = coeffLS(L, S, mother, resonance, gamma)
+        prefactors_ls_max_minus[(L,S)] = factor_second_highest * (Cp_LS - (Cp_l/Cm_l) * Cm_LS)
+    # the highest replacement will depend on the secon highest aswell
+    ls[(lmax-2,smax)] = None
+
+    prefactors_ls_max = {}
+    factor_highest = -1.0 / Cm_l
+    for (L, S) in ls:
+        if L == lmax:
+            prefactors_ls_max[(L, S)] = 0
+            continue
+        cp, cm = coeffLS(L,S, mother, resonance, gamma)
+        prefactors_ls_max[(L, S)] = factor_highest * cm
+
     return prefactors_ls_max, prefactors_ls_max_minus
 
 
@@ -91,7 +104,7 @@ def check_bls(mother:particle,daughter1:particle,daughter2:particle,bls,parity_c
                           Default behaviour will set these two partial waves now!""")
             # here we can try to set the missing ones
             # what we will do is inject a fit parameter of the lambda type, which we will then actually also put in the output yaml
-            # this is a bit of a hack, but it should work
+            # this is a bit of a hack, but it shoulm_lambdad work
             # also this may cause the output to fail due to the rest of this code
 
             # the not gamma is the resonance
@@ -107,16 +120,24 @@ def check_bls(mother:particle,daughter1:particle,daughter2:particle,bls,parity_c
                 raise ValueError(f"""Only the highest and second highest parital waves can be automatically replaced. For the decay {mother} -> {daughter1} {daughter2} this would be
                                  (L,S) = ({max(Lset)}, {max(Sset)}) and ({max(Lset)-2}, {max(Sset)})""")
             
-            prefactors_ls_max, prefactors_ls_max_minus = get_maxLS_prefactors(mother,resonance,gamma,bls, max(Lset), max(Sset))
+            prefactors_ls_max, prefactors_ls_max_minus = get_maxLS_prefactors(mother,resonance,gamma,bls.copy(), max(Lset), max(Sset))
+
             for (L, S), prefactors in zip(missing_L_sorted,[prefactors_ls_max_minus, prefactors_ls_max]):
                 # we will get a lambda parameter for the missing ones
                 # we calculate all the prefactors first and then get the nice lambda parameter string
-                parameter_names = 'abcdefghijklmnop'[:len(bls_frozen.keys())]
-                parameter_times_factor = [f"{name} * {float(prefactors[(L,S)])}" for name, (L, S) in zip(parameter_names, bls_frozen.keys())]
+                parameter_names = 'abcdefghijklmnop'[:len(bls.keys())]
+                parameter_times_factor = [f"{name} * {float(prefactors[(L,S)])}" for name, (L, S) in zip(parameter_names, bls.keys())]
                 name = f"{resonance.name}=>autoSetBLSL:{L},S:{S}"
-                lambda_string = f"lambda({', '.join(parameter_names)}: {' + '.join(parameter_times_factor)}) ({'; '.join([param.name for param in bls_frozen.values()])})"
+                lambda_string = f"lambda({', '.join(parameter_names)}: {' + '.join(parameter_times_factor)}) ({'; '.join([param.name for param in bls.values()])})"
                 print("Setting",name,lambda_string)
                 bls[(L, S)] = lambdaParameter(name, lambda_string)
+            bls_in_dict = {k:np.array(v(numeric=True)) for k,v in bls.items()}
+            h_1_0 = helicity_couplings_from_ls(mother.spin, resonance.spin, gamma.spin,1,0, bls_in_dict)
+            h_mn1_0 = helicity_couplings_from_ls(mother.spin, resonance.spin, gamma.spin,-1,0, bls_in_dict)
+            
+            assert np.allclose(float(abs(h_1_0)**2), 0)
+            assert np.allclose(float(abs(h_mn1_0)**2), 0)
+
     return bls
     
 def flatten(listoflists):
--- AmplitudeCrafter/locals.py
+++ AmplitudeCrafter/locals.py
@@ -1,7 +1,10 @@
 from os.path import dirname, abspath, join
+from logging import getLogger, DEBUG, INFO, WARNING, ERROR, CRITICAL
 import sys
 main_dir = dirname(dirname(abspath(__file__)))
 src_dir = dirname(abspath(__file__))
 config_dir = join(src_dir,"config/")
 decay_example = join(config_dir,"decay_example.yml")
 particle_config = join(config_dir,"particles.yml")
+
+logger = getLogger(__name__)
